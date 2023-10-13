3960b173619c1436a0b732ee1c26e15a2c4b3a62
Johann Dee <johanndee1994@gmail.com>
2023-10-13 16:56:34 -0700

    Add modal

--- src/components/FocusMovie/FocusMovie.js
+++ src/components/FocusMovie/FocusMovie.js
@@ -1,16 +1,22 @@
-function FocusMovie({ focusMovie }) {
+import './FocusMovie.css';
+
+function FocusMovie({ focusMovie, customStyles }) {
+
   const movieDetails = focusMovie.map(movie => {
     console.log('focusMovie', focusMovie);
+    customStyles.content.backgroundImage = `url(${movie.backdrop_path})`
     return (
-      <div className="card">
-        <h2>{movie.title}</h2>
-        <img src={movie.poster_path} className="movie-card-img" />
-        {/* <p>{movieDetails.average_rating.toFixed(2)}</p> */}
-        <p>{movie.release_date}</p>
+      <div className="selected-movie">
+        <img src={movie.poster_path} className="selected-movie-img" />
+        <div className='selected-movie-info'>
+          <h2>{movie.title}</h2>
+          <p>{movie.release_date}</p>
+        </div>
       </div>
     );
   });
-  return <div className="focus-movie-container">{movieDetails}</div>;
+
+  return <div className='modal'>{movieDetails}</div>;
 }
 
 export default FocusMovie;
