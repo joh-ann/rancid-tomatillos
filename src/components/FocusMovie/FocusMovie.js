f86e93f880e13ddead8545a5047995c05f7f41b0
Ann Hochworter <ann.hochworter@gmail.com>
2023-10-12 19:26:21 -0600

    Render single movie

--- src/components/FocusMovie/FocusMovie.js
+++ src/components/FocusMovie/FocusMovie.js
@@ -1,14 +1,16 @@
 function FocusMovie({ focusMovie }) {
-  console.log(focusMovie);
-  return (
-    <div className="card">
-      {/* <h2>{focusMovie}</h2> */}
-      {/* <img src={poster_path} className="movie-card-img" /> */}
-      {/* <h2>{title}</h2>
-      <p>{average_rating.toFixed(2)}</p>
-      <p>{release_date}</p> */}
-    </div>
-  );
+  const movieDetails = focusMovie.map(movie => {
+    console.log('focusMovie', focusMovie);
+    return (
+      <div className="card">
+        <h2>{movie.title}</h2>
+        <img src={movie.poster_path} className="movie-card-img" />
+        {/* <p>{movieDetails.average_rating.toFixed(2)}</p> */}
+        <p>{movie.release_date}</p>
+      </div>
+    );
+  });
+  return <div className="focus-movie-container">{movieDetails}</div>;
 }
 
 export default FocusMovie;
