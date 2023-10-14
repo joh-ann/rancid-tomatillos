f5f75b92766da83aee79fcea9a26b004bff67de1
Ann Hochworter <ann.hochworter@gmail.com>
2023-10-14 14:41:57 -0600

    Continue styling the header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,9 +3,16 @@ import './Header.css';
 function Header() {
   return (
     <div className="page-header">
-      <h1 className="page-title">Rancid Tomatillos</h1>
-      <button className="page-header-btn">Home</button>
-      <button className="page-header-btn">About</button>
+      <div className="title-container">
+        <h1 className="page-title">Rancid Tomatillos</h1>
+      </div>
+      <div className="header-btns-container">
+        <button className="page-header-btn">Home</button>
+        <button className="page-header-btn">About</button>
+      </div>
+      <div className="search-btn-container">
+        <button className="page-header-btn">Search</button>
+      </div>
     </div>
   );
 }
