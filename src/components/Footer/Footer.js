7fb992f4d53fa73fc31dc36c0669a285295f684a
Ann Hochworter <ann.hochworter@gmail.com>
2023-10-14 14:01:56 -0600

    Continue styling the footer components

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,22 +6,30 @@ function Footer() {
       <div className="title-block">
         <h2 className="subtitle">Rancid Tomatillos</h2>
         <p className="project-details">
-          Rancid Tomatillos is a paired Turing project assigned during the first
-          week of Module 3. It is our first React.js project. This version is
+          Rancid Tomatillos is a Turing project assigned during the first week
+          of Module 3. It is our first React.js project. This version is
           designed and developed by Johann Dee and Ann Hochworter.
         </p>
       </div>
       <div className="linkedin">
         <h2 className="subtitle">LinkedIn</h2>
-        <h3>
-          <a href="https://linkedin/in/johanndee">Johann Dee</a>
-        </h3>
-        <h3>
-          <a href="https://linkedin/in/annhochworter">Ann Hochworter</a>
-        </h3>
+        <p>
+          <a href="https://www.linkedin.com/in/johanndee/">Johann Dee</a>
+        </p>
+        <p>
+          <a href="https://www.linkedin.com/in/annhochworter/">
+            Ann Hochworter
+          </a>
+        </p>
       </div>
       <div className="github">
         <h2 className="subtitle">GitHub</h2>
+        <p>
+          <a href="https://github.com/joh-ann">github.com/joh-ann</a>
+        </p>
+        <p>
+          <a href="https://github.com/AHochworter">github.com/AHochworter</a>
+        </p>
       </div>
     </div>
   );
