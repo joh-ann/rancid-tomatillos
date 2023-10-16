96d125e4ac17092fa7f5288b4a1c2d757e5338d8
Johann Dee <johanndee1994@gmail.com>
2023-10-16 00:28:15 -0700

    Update footer styling

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,10 +6,9 @@ function Footer() {
       <div className="title-block">
         <h2 className="subtitle">Rancid Tomatillos</h2>
         <p className="project-details">
-          Rancid Tomatillos is a Turing project assigned during the first week
-          of Module 3. It is our first React.js project. This version is
-          designed and developed by Johann Dee and Ann Hochworter.
+          Rancid Tomatillos is a Turing School Module 3 project built with React.js. Designed and developed by FE 2307 Johann Dee and Ann Hochworter.
         </p>
+        <p className='project-details'><a href="https://github.com/joh-ann/rancid-tomatillos">github.com/joh-ann/rancid-tomatillos</a></p>
       </div>
       <div className="linkedin">
         <h2 className="subtitle">LinkedIn</h2>
