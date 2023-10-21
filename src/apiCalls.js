6c8c5a54a5b47111eb9a1eaa71a1e852296f61ae
Johann Dee <johanndee1994@gmail.com>
2023-10-20 19:47:00 -0700

    Update error message styling

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,12 +1,12 @@
 export function getAllMovies() {
-  return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies').then(
+  return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movis').then(
     response => {
       if (!response.ok) {
         if (response.status === 404) {
-          throw new Error('Sorry, the movies were not found.');
+          throw new Error('Sorry, no movies were not found.');
         } else if (response.status === 500) {
           throw new Error(
-            'Oops, something went wrong on our server. Please try again later.'
+            'Oops! Something went wrong on our server. Please try again later.'
           );
         } else {
           throw new Error('An error occurred while fetching movies.');
